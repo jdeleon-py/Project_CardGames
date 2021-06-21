16ec5b2865af33bedf39382c09c0e8a863fca174
JamesdeLeon <jdeleonphys@gmail.com>
2021-06-21 11:38:54 -0500

    added test program; modified inherited __init__()

--- blackjack/src/dealer.py
+++ blackjack/src/dealer.py
@@ -14,14 +14,24 @@ class Dealer(Hand):
 		- (inherited) hand of cards
 	'''
 	def __init__(self):
-		super().__init__(self)
+		Hand.__init__(self)
 		self.deck = Deck()
 	
 
 	def deal_card(self):
-		pass
-
+		return self.deck.deck.pop(0)
 
 
 if __name__ == "__main__":
-	pass
+	dealer = Dealer()
+
+	# Three random cards are dealt
+	for i in range(0, 3): print("Card #{0}: {1}".format(i + 1, dealer.deal_card()))
+
+	# Dealer deals cards that are then added to their own hand
+	dealer.add_card(dealer.deal_card())
+	dealer.add_card(dealer.deal_card())
+	dealer.add_card(dealer.deal_card())
+
+	# Cards in hand are displayed
+	dealer.display_hand()
