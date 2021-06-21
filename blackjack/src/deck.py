c437209efdeb1588a4880a5acf23c30ebcb5ef0d
JamesdeLeon <jdeleonphys@gmail.com>
2021-06-21 11:38:00 -0500

    removed redundant method

--- blackjack/src/deck.py
+++ blackjack/src/deck.py
@@ -7,7 +7,6 @@ class Deck:
 	'''
 	METHODS:
 		- ability to randomly shuffle the array of card objects
-		- ability to remove a card object from the array (deal card)
 
 	ATTRIBUTES:
 		- array of card objects (every combination of card objects)
@@ -25,13 +24,7 @@ class Deck:
 		return random.shuffle(self.deck)
 
 
-	def deal_card(self):
-		return self.deck.pop(0)
-
-
 if __name__ == "__main__":
 	new_deck = Deck()
 
-	for i in range(0, 3): print("Card #{0}: {1}".format(i + 1, new_deck.deal_card()))
-	print("\n")
-	print("{} cards left in the deck!".format(len(new_deck.deck)))
+	for i in range(0, 30): print("Card #{0}: {1}".format(i + 1, new_deck.deck[i]))
